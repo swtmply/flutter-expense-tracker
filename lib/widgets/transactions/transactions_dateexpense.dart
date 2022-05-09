2c0fb8950e68725ddd863c3172f3a18e41ffdcd7
swtmply <delosreyesjohnallen@gmail.com>
2022-05-09 13:07:15 +0800

    added date expense in trasactions list

--- lib/widgets/transactions/transactions_dateexpense.dart
+++ lib/widgets/transactions/transactions_dateexpense.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gas2s/models/transaction/transaction_model.dart';
 import 'package:gas2s/theme/colors.dart';
 import 'package:gas2s/widgets/transactions/transaction_list_item.dart';
+import 'package:intl/intl.dart';
 
 class TransactionDateExpense extends StatelessWidget {
   const TransactionDateExpense(
@@ -12,15 +13,66 @@ class TransactionDateExpense extends StatelessWidget {
   final int? amount;
   final List<Transaction> transactionListItems;
 
+  // ignore: todo
+  // TODO work for a better algorithm
+
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, int>?> transactionDates =
+        transactionListItems.reversed
+            .map((transaction) {
+              return DateFormat('MMM dd, yyyy')
+                  .format(transaction.dateAdded)
+                  .toString();
+            })
+            .toSet()
+            .toList()
+            .map((date) {
+              int amount = 0;
+
+              for (var transaction in transactionListItems) {
+                if (DateFormat('MMM dd, yyyy')
+                        .format(transaction.dateAdded)
+                        .toString() ==
+                    date) {
+                  amount = transaction.isExpense
+                      ? amount - transaction.amount.toInt()
+                      : amount + transaction.amount.toInt();
+                }
+              }
+
+              return {date: amount};
+            })
+            .toList();
+
     return Expanded(
       child: ListView.builder(
-        itemCount: transactionListItems.length,
+        itemCount: transactionDates.length,
         itemBuilder: (context, index) {
-          return TransactionListItem(
-              transaction: transactionListItems[
-                  transactionListItems.length - (index + 1)]);
+          return Column(
+            children: [
+              DateExpense(
+                date: transactionDates[index]!.keys.first.toString(),
+                amount: transactionDates[index]!.values.first,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ...transactionListItems.toList().reversed.map((transaction) {
+                if (DateFormat('MMM dd, yyyy')
+                        .format(transaction.dateAdded)
+                        .toString() ==
+                    transactionDates[index]!.keys.first.toString()) {
+                  return TransactionListItem(transaction: transaction);
+                }
+
+                return Container();
+              }),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          );
         },
       ),
     );
@@ -50,7 +102,7 @@ class DateExpense extends StatelessWidget {
           ),
         ),
         Text(
-          amount.toString(),
+          NumberFormat.simpleCurrency(locale: 'fil').format(amount),
           style: const TextStyle(
             color: AppColors.coolGray,
           ),
